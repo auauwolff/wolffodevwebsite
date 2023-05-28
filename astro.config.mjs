9166f7f6adb2a5cd37be351abfdade457df0d9b7
auauwolff <auauwolff@gmail.com>
2023-05-28 17:16:06 +1000

    Experimental image asset astro test

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,15 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
-import image from "@astrojs/image";
 
 import netlify from "@astrojs/netlify/functions";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react(), image()],
+  integrations: [tailwind(), react()],
   output: "server",
-  adapter: netlify()
+  adapter: netlify(),
+  experimental: {
+    assets: true,
+  },
 });
