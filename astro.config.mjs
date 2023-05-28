a13cb2f58637211cddb4ad0a0d87967b6ca71141
auauwolff <auauwolff@gmail.com>
2023-05-28 10:45:56 +1000

    netlify integration added

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,13 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
-
 import image from "@astrojs/image";
+
+import netlify from "@astrojs/netlify/functions";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react(), image()]
+  integrations: [tailwind(), react(), image()],
+  output: "server",
+  adapter: netlify()
 });
