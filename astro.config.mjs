af9419556fc7a1c619c951287a2e21690ecde081
auauwolff <auauwolff@gmail.com>
2023-06-03 15:14:53 +1000

    back to wha it was

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import netlify from "@astrojs/netlify/functions";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react()],
-  output: "static",
+  output: "server",
   adapter: netlify(),
   experimental: {
     assets: true,
