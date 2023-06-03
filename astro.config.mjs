f3c53267d887136363ca27fe8766a5c71bdb2331
auauwolff <auauwolff@gmail.com>
2023-06-03 15:13:00 +1000

    astro static

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import netlify from "@astrojs/netlify/functions";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react()],
-  output: "server",
+  output: "static",
   adapter: netlify(),
   experimental: {
     assets: true,
