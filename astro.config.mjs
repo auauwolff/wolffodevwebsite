e15d112eff76a4a7f44492c1aa76093abecc4e75
auauwolff <auauwolff@gmail.com>
2023-06-03 18:56:42 +1000

    sitemap added to astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,12 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()],
+  site: "https://duit.solutions/",
+  integrations: [tailwind(), react(), sitemap()],
   output: "static",
   experimental: {
     assets: true,
